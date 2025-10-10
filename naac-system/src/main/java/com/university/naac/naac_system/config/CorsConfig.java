c34d17415b049c3b9a1154f48f0087663732d52f
Harshit Atri <harshitatri74@gmail.com>
2025-10-10 22:59:56 +0530

    Added CORS Config to interact with frontend on localHost.

---
+++ naac-system/src/main/java/com/university/naac/naac_system/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.university.naac.naac_system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // apply to all endpoints
+                        .allowedOrigins("http://localhost:3000") // your React app origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
